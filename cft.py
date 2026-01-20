88b346363968d10bbed9bf4346dcbbb157046ead
kitunyacell-kun <kitunyacell@gmail.com>
2026-01-20 13:10:01 +0700

    Update cft.py

--- cft.py
+++ cft.py
@@ -111,7 +111,7 @@ while True:
             sys.exit("Invalid Number Of Packets!")
         u = udp(ip,port,size,packets)
         u.start()
-        print("\033[33m[\033[1m+\033[33m] \033[92mRequest " +str(bytes)+ "  \033[33mto Sent attack \033[97m  \033[96m-->  \033[95m" +ip+ " \033[0m" )
+        print("\033[33m[\033[1m+\033[33m] \033[92mRequest " +str(ip)+ "  \033[33mto Sent attack \033[97m  \033[96m-->  \033[95m" +port+ " \033[0m" )
         time.sleep(1)
     except KeyboardInterrupt:
         print ("Stopping Flood!")
